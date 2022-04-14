40aed1cd880b90b8d9c8e2a20d956f799ca18480
Vladislav <64910163+bo1oto@users.noreply.github.com>
2022-04-14 11:22:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from logic import start_telegram_bot
 
 if __name__ == '__main__':
-    start_telegram_bot()
+    try:
+        start_telegram_bot()
+    except Exception as exc:
+        with open('\\error.txt', 'wt') as file:
+            file.write(str(exc))
